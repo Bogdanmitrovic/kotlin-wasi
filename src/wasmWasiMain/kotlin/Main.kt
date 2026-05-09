0ba6960a43a3c830ee13078550d3780509d53d1d
Bogdan Mitrović <bogdanmitrovic17102001@gmail.com>
2026-05-09 18:41:06 +0200

    Fix: remove old readFromStdin

--- src/wasmWasiMain/kotlin/Main.kt
+++ src/wasmWasiMain/kotlin/Main.kt
@@ -8,34 +8,15 @@ import kotlin.wasm.unsafe.withScopedMemoryAllocator
 @WasmImport("wasi_snapshot_preview1", "fd_read")
 private external fun wasiFdRead(fd: Int, iovs: Int, iovsLength: Int, bytesReadPtr: Int): Int
 
-@OptIn(UnsafeWasmMemoryApi::class)
-fun readFromStdin(): String? = withScopedMemoryAllocator { allocator ->
-    val bufSize = 1024
-    val buffer = allocator.allocate(bufSize)
-    val iov = allocator.allocate(8)
-    val bytesReadPtr = allocator.allocate(4)
-
-    Pointer(iov.address).storeInt(buffer.address.toInt())
-    Pointer((iov.address + 4u)).storeInt(bufSize)
-
-    val err = wasiFdRead(0, iov.address.toInt(), 1, bytesReadPtr.address.toInt())
-    if (err != 0) return@withScopedMemoryAllocator null
-    val bytesRead = Pointer(bytesReadPtr.address).loadInt()
-    if (bytesRead == 0) return@withScopedMemoryAllocator null
-
-    val bytes = ByteArray(bytesRead)
-    for (i in 0 until bytesRead) {
-        bytes[i] = Pointer((buffer.address + i.toUInt())).loadByte()
-    }
-    bytes.decodeToString()
-}
-
 private fun readLine(): String? {
     val sb = StringBuilder()
     while (true) {
         val b = readByte()
         if (b == -1) return null
-        if (b == '\n'.code) return sb.toString()
+        if (b == '\n'.code) {
+            if (sb.isNotEmpty() && sb.last() == '\r') sb.deleteAt(sb.length - 1)
+            return sb.toString()
+        }
         sb.append(b.toChar())
     }
 }
